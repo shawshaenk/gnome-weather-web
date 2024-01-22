325131dc70d413c61fd373a74aa6806617a1d7c9
shawshaenk <138606372+shawshaenk@users.noreply.github.com>
2024-01-22 00:28:56 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 const apiKey = "ba87e1f04c9ccbe4f57b4cb024da6d64";
+
 let cityBox = document.getElementById("cityBox");
 let conditions = document.getElementById("conditions");
 let temperature = document.getElementById("temperature");
-let cityName = "New York City";
+let cityDisplayName = document.getElementById("cityDisplayName")
+
+let tempBlock = document.getElementById('tempBlock');
+let conditionsBlock = document.getElementById('conditionsBlock');
+let cityBlock = document.getElementById('cityBlock');
 
 async function getWeather() {
-    cityName = cityBox.value;
+    let cityName = cityBox.value;
+    if (cityName === "") {
+        cityName = "New York City";
+    }
     response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
     data = await response.json();
     conditions.innerHTML = data.weather[0].main;
     temperature.innerHTML = Math.floor(data.main.temp) + "°F";
+    cityDisplayName.innerHTML = cityName;
 }
 getWeather();
 
-cityBox.onkeydown = function animateBlocks() {
-    getWeather();
+cityBox.addEventListener("keydown", function(event) {
+    if (event.code === "Enter") {
+        getWeather();
 
-    let tempBlock = document.getElementById('tempBlock');
-    tempBlock.style.animation = "none"
+        tempBlock.style.animation = "none";
+        conditionsBlock.style.animation = "none";
+        cityBlock.style.animation = "none";
 
-    let conditionsBlock = document.getElementById('conditionsBlock');
-    conditionsBlock.style.animation = "none"
-
-    requestAnimationFrame(() => {
-        tempBlock.style.animation="zoomInAndOut 0.4s forwards";
-        conditionsBlock.style.animation="zoomInAndOut 0.4s forwards"
-    });
-}
+        requestAnimationFrame(() => {
+            tempBlock.style.animation = "zoomInAndOut 0.4s forwards";
+            conditionsBlock.style.animation = "zoomInAndOut 0.4s forwards"
+            cityBlock.style.animation = "zoomInAndOut 0.4s forwards"
+        });
+    }
+});
