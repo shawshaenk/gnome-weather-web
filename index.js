3c27b520c91761befdb3464c6667c09285a68870
shawshaenk <138606372+shawshaenk@users.noreply.github.com>
2024-01-24 23:55:06 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const apiKey = "ba87e1f04c9ccbe4f57b4cb024da6d64";
 let cityBox = document.getElementById("cityBox");
 let conditions = document.getElementById("conditions");
 let temperature = document.getElementById("temperature");
-let cityDisplayName = document.getElementById("cityDisplayName")
+let realCityName = document.getElementById("realCityName");
 
 let tempBlock = document.getElementById('tempBlock');
 let conditionsBlock = document.getElementById('conditionsBlock');
@@ -17,9 +17,10 @@ async function getWeather() {
     }
     response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
     data = await response.json();
+    console.log(data);
     conditions.innerHTML = data.weather[0].main;
     temperature.innerHTML = Math.floor(data.main.temp) + "°F";
-    cityDisplayName.innerHTML = cityName;
+    realCityName.innerHTML = data.name;
 }
 getWeather();
 
@@ -33,8 +34,8 @@ cityBox.addEventListener("keydown", function(event) {
 
         requestAnimationFrame(() => {
             tempBlock.style.animation = "zoomInAndOut 0.4s forwards";
-            conditionsBlock.style.animation = "zoomInAndOut 0.4s forwards"
-            cityBlock.style.animation = "zoomInAndOut 0.4s forwards"
+            conditionsBlock.style.animation = "zoomInAndOut 0.4s forwards";
+            cityBlock.style.animation = "zoomInAndOut 0.4s forwards";
         });
     }
 });
